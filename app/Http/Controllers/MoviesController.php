bf9dec7e81a44bd29640df92504ab15dc0f9c625
SaidShahAhmadi <saidshahahmadi2016@gmail.com>
2020-07-07 23:55:25 -0700

    Update MoviesController.php

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -21,11 +21,11 @@ class MoviesController extends Controller
     
         $popularMovies = Http::withToken(config('services.tmdb.token'))
         ->get('https://api.themoviedb.org/3/movie/popular')
-        ->json()["results"];
+        ->json();
 
         $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
         ->get('https://api.themoviedb.org/3/movie/now_playing')
-        ->json()["results"];
+        ->json();
 
         $genres= Http::withToken(config('services.tmdb.token'))
         ->get('https://api.themoviedb.org/3/genre/movie/list')
